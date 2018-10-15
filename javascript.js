2100daf1aa75dd6331dc236c32233620eb325bc7
Austin Wayman <awguitar@msn.com>
2018-10-15 09:59:55 -0700

    fixed gifs

--- javascript.js
+++ javascript.js
@@ -2,47 +2,10 @@ $(document).ready(function() {
     //Array for searched topics to be added
     var comedians = [];
     
-        //Function with AJAX call to GIPHY; Q parameterc for API link set to search term, limit 10 results
-      //Create div with respective still and animate image sources with "data-state", "data-still" and "data-animate" attributes
-         function displayComedianName() {
+      
+      
     
-        var comedian = $(this).data("search");
-        console.log(comedian);
-    
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + comedian + "&api_key=SzR8GH72ppHulK01VqcC09jcc10UyZoy&limit=1";
-    
-        console.log(queryURL);
-    
-        $.ajax({
-              url: queryURL,
-              method: "GET"
-            }).done(function(response) {
-                var results = response.data;
-                console.log(results);
-                for (var i = 0; i < results.length; i++) {
-                
-                var comDiv = $("<div class='col-md-4'>");
-    
-                var rating = results[i].rating;
-                var defaultAnimatedSrc = results[i].images.fixed_height.url;
-                var staticSrc = results[i].images.fixed_height_still.url;
-                var showImage = $("<img>");
-                var p = $("<p>").text("Rating: " + rating);
-    
-                showImage.attr("src", staticSrc);
-                showImage.addClass("comedianGiphy");
-                showImage.attr("data-state", "still");
-                showImage.attr("data-still", staticSrc);
-                showImage.attr("data-animate", defaultAnimatedSrc);
-                comDiv.append(p);
-                comDiv.append(showImage);
-                $("#gifs-appear-here").prepend(comDiv);
-    
-            }
-        });
-    }
-    
-      //Submit button click event takes search term from form input, trims and pushes to topics array, displays button
+      //Submit button click event takes search term from form input, trims and pushes to comedians array, displays button
         $("#addCom").on("click", function(event) {
             event.preventDefault();
             var newCom = $("#comedianInput").val().trim();
@@ -52,12 +15,11 @@ $(document).ready(function() {
             displayButtons();
           });
     
-      //Function iterates through topics array to display button with array values in "myButtons" section of HTML
+      //Function iterates through comedians array to display button with array values in "myButtons" section of HTML
         function displayButtons() {
         $("#myButtons").empty();
         for (var i = 0; i < comedians.length; i++) {
           var a = $('<button class="btn btn-primary">');
-          a.attr("id", "show");
           a.attr("data-comedian", comedians[i]);
           a.text(comedians[i]);
           $("#myButtons").append(a);
@@ -67,10 +29,47 @@ $(document).ready(function() {
     
       displayButtons();
     
-      //Click event on button with id of "show" executes displayNetflixShow function
-      $(document).on("click", "#show", displayComedianName);
+      //Click event on button with id of "show" executes displayComedianName function
+      $(document).on("click", ".btn", function(){
     
-      //Click event on gifs with class of "netflixGiphy" executes pausePlayGifs function
+          var comedian = $(this).data("comedian");
+          console.log(comedian);
+      
+          var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + comedian + "&api_key=SzR8GH72ppHulK01VqcC09jcc10UyZoy&limit=1";
+      
+          console.log(queryURL);
+      
+          $.ajax({
+                url: queryURL,
+                method: "GET"
+              }).done(function(response) {
+                  var results = response.data;
+                  console.log(results);
+                  for (var i = 0; i < results.length; i++) {
+                  
+                  var comDiv = $("<div class='col-md-4'>");
+      
+                  var rating = results[i].rating;
+                  var defaultAnimatedSrc = results[i].images.fixed_height.url;
+                  var staticSrc = results[i].images.fixed_height_still.url;
+                  var showImage = $("<img>");
+                  var p = $("<p>").text("Rating: " + rating);
+      
+                  showImage.attr("src", staticSrc);
+                  showImage.addClass("comedianGiphy");
+                  showImage.attr("data-state", "still");
+                  showImage.attr("data-still", staticSrc);
+                  showImage.attr("data-animate", defaultAnimatedSrc);
+                  comDiv.append(p);
+                  comDiv.append(showImage);
+                  $("#gifs-appear-here").prepend(comDiv);
+      
+              }
+          });
+      
+      });
+    
+      //Click event on gifs with class of "comedianGiphy" executes pausePlayGifs function
       $(document).on("click", ".comedianGiphy", pausePlayGifs);
     
       //Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-still"
